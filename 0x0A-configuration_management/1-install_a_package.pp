9e1a2ec53d1407e279480c8a9a5054d3cf6b7d0d
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-04 11:20:49 +0300

    removed devops

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,5 +0,0 @@
-# Install a package flask from pip3
-exec {'install_flask':
-    command => 'pip3 install flask==2.1.0',
-    path    => ['/usr/bin']
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,5 +0,0 @@
-# Kill a commd
-exec {'exec_command':
-    command => 'pkill killmenow',
-    path    => ['/bin', '/usr/bin']
-}
