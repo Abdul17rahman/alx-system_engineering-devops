66a47329788f6d23fc186b84e3d770e668c61511
Abdul Rahman <abdulnsamba@gmail.com>
2023-11-25 00:25:37 +0300

    wez

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,9 @@
 # This file installs flask using puppet 
 package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
+}
+package {'Werkzeug':
+  ensure   => 'Werkzeug 2.1.1',
+  provider => 'pip3'
 }
