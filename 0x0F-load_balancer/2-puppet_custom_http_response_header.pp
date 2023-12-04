9f90760168435174111b3be53cc9696cb236ecc6
Abdul Rahman <abdulnsamba@gmail.com>
2023-12-05 01:17:02 +0300

    header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,22 @@
+# Install and configure an Nginx server using Puppet instead of Bash
+exec {'apt-update':
+  command => '/usr/bin/apt-get update'
+}
+
+# Install nginx package
+package {'nginx':
+  require => Exec['apt-update'],
+  ensure  => installed,
+}
+
+# Add a custom header
+exec {'http_header':
+  user      => root,
+  command   => "sudo sed -i "/server_name _/a \\    add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default",
+}
+
+# Restart Nginx server
+exec {'nginx_restart':
+  user    => root,
+  command => '/sbin/service nginx start',
+}
