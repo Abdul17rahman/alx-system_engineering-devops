2be42b130b012d98f2f459f95cb51635502af143
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-03 15:05:32 +0300

    data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-    Get data from an APi using requests
+Get data from an APi using requests
 """
 
 
@@ -12,19 +12,26 @@ import json
 
 session = HTMLSession()
 user_id = int(sys.argv[1])
-
-# get user data
 url = 'https://jsonplaceholder.typicode.com'
-user = session.get(f'{url}/users/{user_id}').json()
 
-# get todos
-todos = session.get(f'{url}/todos?userId={user_id}').json()
+
+def get_user(user_id, url):
+    """ Function that gets user data """
+    return session.get(f'{url}/users/{user_id}').json()
+
+
+def get_todos(user_id, url):
+    """ Fuction that todo data"""
+    return session.get(f'{url}/todos?userId={user_id}').json()
+
+
+user = get_user(user_id, url)
+todos = get_todos(user_id, url)
 
 completed = [todo for todo in todos if todo['completed']]
 emp_name = user['name']
 print(f'Employee {emp_name} is done with tasks({len(completed)}/{len(todos)})')
 
-# Print completed todos
 for todo in completed:
     title = todo['title']
     print(f'\t{title}')
