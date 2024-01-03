20cc013a767d7e9b93ac14a8eecd2d88483cada9
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-03 15:14:48 +0300

    doc

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Get data from an APi using requests
 """
@@ -15,23 +14,24 @@ user_id = int(sys.argv[1])
 url = 'https://jsonplaceholder.typicode.com'
 
 
-def get_user(user_id, url):
-    """ Function that gets user data """
-    return session.get(f'{url}/users/{user_id}').json()
+if __name__ == "__main__":
+    def get_user(user_id, url):
+        """ Function that gets user data """
+        return session.get(f'{url}/users/{user_id}').json()
 
 
-def get_todos(user_id, url):
-    """ Fuction that todo data"""
-    return session.get(f'{url}/todos?userId={user_id}').json()
+    def get_todos(user_id, url):
+        """ Fuction that todo data"""
+        return session.get(f'{url}/todos?userId={user_id}').json()
 
 
-user = get_user(user_id, url)
-todos = get_todos(user_id, url)
+    user = get_user(user_id, url)
+    todos = get_todos(user_id, url)
 
-completed = [todo for todo in todos if todo['completed']]
-emp_name = user['name']
-print(f'Employee {emp_name} is done with tasks({len(completed)}/{len(todos)})')
+    completed = [todo for todo in todos if todo['completed']]
+    emp_name = user['name']
+    print(f'Employee {emp_name} is done with tasks({len(completed)}/{len(todos)})')
 
-for todo in completed:
-    title = todo['title']
-    print(f'\t{title}')
+    for todo in completed:
+        title = todo['title']
+        print(f'\t{title}')
