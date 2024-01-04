3a38609315f1280ec5d09f01dc03d4996ddad651
Abdul Rahman <abdulnsamba@gmail.com>
2024-01-04 17:04:44 +0300

    req

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,21 +9,20 @@
 
 
 from sys import argv
-from requests_html import HTMLSession
+import requests
 
 
 if __name__ == "__main__":
-    session = HTMLSession()
     user_id = int(argv[1])
     url = 'https://jsonplaceholder.typicode.com'
 
     def get_user(_id, _url):
         """ Function that gets user data """
-        return session.get(f'{_url}/users/{_id}').json()
+        return requests.get(f'{_url}/users/{_id}').json()
 
     def get_todos(_id, _url):
         """ Fuction that todo data"""
-        return session.get(f'{_url}/todos?userId={_id}').json()
+        return requests.get(f'{_url}/todos?userId={_id}').json()
 
     user = get_user(user_id, url)
     todos = get_todos(user_id, url)
